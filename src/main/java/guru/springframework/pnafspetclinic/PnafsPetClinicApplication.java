b08747feebe84b51b40f9787d04477bcff6263bb
pankajnafria <inafsdevelope@gmail.com>
2021-08-26 17:03:42 -0700

    Initial Project Add

---
+++ src/main/java/guru/springframework/pnafspetclinic/PnafsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.pnafspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnafsPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnafsPetClinicApplication.class, args);
+	}
+
+}
