f7ae4a97de04e3b087c02ac592ea540aaac2230f
pankajnafria <inafsdevelope@gmail.com>
2021-10-01 17:45:40 -0700

    Added BaseEntity. Closes #11.

--- pet-clinic-data/src/main/java/guru/springframework/pnafspetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/pnafspetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package guru.springframework.pnafspetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
