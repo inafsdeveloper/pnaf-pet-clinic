cdbd1af9b97a07ea273a610d10ffbf0184b48786
pankajnafria <inafsdevelope@gmail.com>
2021-10-11 13:45:08 -0700

    Added PetType Map Service. Close #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/pnafspetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.pnafspetclinic.services;
+
+import guru.springframework.pnafspetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
