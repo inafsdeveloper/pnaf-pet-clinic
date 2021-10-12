c567f90eccf64b0e0ccc6b9bf48a8c930db91350
pankajnafria <inafsdevelope@gmail.com>
2021-10-12 11:32:28 -0700

    Added Visit Map Service. Closes #47.

---
+++ pet-clinic-data/src/main/java/guru/springframework/pnafspetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.pnafspetclinic.services;
+
+import guru.springframework.pnafspetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
