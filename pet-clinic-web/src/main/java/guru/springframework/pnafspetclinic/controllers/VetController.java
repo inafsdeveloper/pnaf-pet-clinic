514429de8df2b6354d31c64548322747631780a2
pankajnafria <inafsdevelope@gmail.com>
2021-10-12 10:42:33 -0700

    Fixed broken links. Closes #46.

--- pet-clinic-web/src/main/java/guru/springframework/pnafspetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/pnafspetclinic/controllers/VetController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/vets"})
 @Controller
 public class VetController {
     private final VetService vetService;
@@ -14,9 +13,24 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/index", "/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
+        System.out.println("List of Vets");
+        vetService.findAll().forEach(vet -> {
+            System.out.println(
+                    vet.getId() + ", " +
+                            vet.getFirstName() + ", " +
+                            vet.getLastName()
+            );
+            System.out.println("    List of Vet's Specialities");
+            vet.getSpecialities().forEach(
+                    speciality ->
+                            System.out.println("    " +
+                                    speciality.getId() + ", " +
+                                    speciality.getDescription())
+            );
+        });
         return "vets/index";
     }
 }
